6981899d336369d6ac2e4ae3304f082f58a2bf5f
bre-nda <brenda.kanale@student.moringaschool.com>
2021-10-24 23:25:44 +0300

    create a subclass testcredentials

--- credentials_test.py
+++ credentials_test.py
@@ -1,27 +1,12 @@
 import unittest
 from credentials import Credentials
 
-class TestCredentials (unittest.TestCase):
+class TestCredentials(unittest.TestCase):
+
     '''
-    Test class that defines test cases for the credential class behaviours.
+    Test class that defines test cases for the credentials class behaviours.
 
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
-    def setUp(self):
-        '''
-        Set up method to run before each test cases.
-        '''
-        self.new_credentials = Credentials("user_name","password","email")
     
-    def test_init(self):
-        '''
-        test_init test case to test if the object is initialized properly
-        '''
-        self.assertEqual(self.new_credentials.user_name,"user_name")
-        self.assertEqual(self.new_credentials.password,"password")
-        self.assertEqual(self.new_credentials.email,"email")
-
-        
-if __name__ == '__main__':
-    unittest.main()
