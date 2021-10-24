47f84639e9ceec6164debd3e02e3b4ca6928caa3
bre-nda <brenda.kanale@student.moringaschool.com>
2021-10-24 21:34:34 +0300

    create testcase  for the credentials

--- credentials_test.py
+++ credentials_test.py
@@ -1,1 +1,10 @@
 import unittest
+from credentials import Credentials
+
+class TestCredentials (unittest.TestCase):
+    '''
+    Test class that defines test cases for the credential class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
