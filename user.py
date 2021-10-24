78752ee92d9ebec6d6038df67148eefaa7ef5958
bre-nda <brenda.kanale@student.moringaschool.com>
2021-10-25 01:45:05 +0300

    created class user

--- user.py
+++ user.py
@@ -1,5 +1,14 @@
 class User:
-    '''
-    Class that generates new instances of a User
-    '''
-    
+    """
+    Class that generates new instances of users.
+    """
+
+    user_list = [] 
+
+    def __init__(self,full_name,user_name,email):
+
+      # docstring removed for simplicity
+
+        self.full_name = full_name
+        self.user_name = user_name
+        self.email = email
