f65ca6daf74e852de7371960e0cd6dac9a4e9421
bre-nda <brenda.kanale@student.moringaschool.com>
2021-10-24 23:34:37 +0300

    passed first test

--- credentials_test.py
+++ credentials_test.py
@@ -9,4 +9,20 @@ class TestCredentials(unittest.TestCase):
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
-    
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_credentials = Credentials("bree","bren","brendaandeso4@gmail.com") 
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+        self.assertEqual(self.new_credentials.user_name,"bree")
+        self.assertEqual(self.new_credentials.password,"bren")
+        self.assertEqual(self.new_credentials.email,"brendaandeso4@gmail.com") 
+
+        
+if __name__ == '__main__':
+    unittest.main()
