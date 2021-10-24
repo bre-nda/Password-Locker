59e704a5e46035f1598506a9a68d9ba60b820a97
bre-nda <brenda.kanale@student.moringaschool.com>
2021-10-24 23:53:54 +0300

    initialized git

--- credentials.py
+++ credentials.py
@@ -1,19 +1,28 @@
 class Credentials:
     """
-    class that generates new instances of credentials
+    Class that generates new instances of credentials.
     """
 
-pass
-def __init__(self,user_name,password,email):
+    credential_list = [] # Empty contact list
 
+    def __init__(self,user_name,password,email):
         '''
         __init__ method that helps us define properties for our objects.
 
         Args:
-            user_name : New credential user_name.
-            password: New credential password.
-            email : New credentials email address.
+            first_name: New contact first name.
+            last_name : New contact last name.
+            number: New contact phone number.
+            email : New contact email address.
         '''
         self.user_name = user_name
         self.password = password
         self.email = email
+
+    def save_credentials(self):
+
+        '''
+        save_credentials method saves credentials objects into credential_list
+        '''
+
+        Credentials.credential_list.append(self)
