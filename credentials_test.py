e2891ee05a73ed526a0aef97f70a9c887b0dd856
bre-nda <brenda.kanale@student.moringaschool.com>
2021-10-24 21:51:32 +0300

    create a setup method

--- credentials_test.py
+++ credentials_test.py
@@ -8,3 +8,9 @@ class TestCredentials (unittest.TestCase):
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_credentials = Credentials("Bree","b1234","brendaandeso4@gmail.com")
+        
