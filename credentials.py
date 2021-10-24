f0a2de9d373590dc48d6bfc4e0ac5f8da5af1f9d
bre-nda <brenda.kanale@student.moringaschool.com>
2021-10-24 20:39:10 +0300

    create a class credentials

---
+++ credentials.py
@@ -0,0 +1,17 @@
+class Credentials:
+    """
+    class that generates new instances of credentials
+    """
+
+pass
+def __init__(self,user_name,password,email):
+
+        '''
+        __init__ method that helps us define properties for our objects.
+
+        Args:
+            first_name: New contact first name.
+            last_name : New contact last name.
+            number: New contact phone number.
+            email : New contact email address.
+        '''
