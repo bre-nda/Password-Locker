61c651d0aedd1be4e488d4f7ceba23fc6665bc46
bre-nda <brenda.kanale@student.moringaschool.com>
2021-10-25 08:13:51 +0300

    did the first class

--- user_test.py
+++ user_test.py
@@ -1,2 +1,29 @@
 import unittest
 from user import User
+
+class TestUser(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the contact class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_user = User("brenda andeso","bree","brendaandeso4@gmail.com")
+    
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_user.full_name,"brenda andeso")
+        self.assertEqual(self.new_user.user_name,"bree")
+        self.assertEqual(self.new_contact.email,"brendaandeso4@gmail.com")
+
+
+if __name__ == '__main__':
+    unittest.main()
