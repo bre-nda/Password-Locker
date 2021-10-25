a9a63a3d83c90c80482b482fcc7b6178ac0df141
bre-nda <brenda.kanale@student.moringaschool.com>
2021-10-25 11:46:28 +0300

    called out functions for various tests

--- user.py
+++ user.py
@@ -7,8 +7,39 @@ class User:
 
     def __init__(self,full_name,user_name,email):
 
-      # docstring removed for simplicity
 
         self.full_name = full_name
         self.user_name = user_name
         self.email = email
+
+    user_list = [] 
+    def save_user(self):
+
+        '''
+        save_user method saves user objects into user_list
+        '''
+
+        User.user_list.append(self)
+
+    def delete_user(self):
+
+        '''
+        delete_user method deletes a saved user from the user_list
+        '''
+
+        User.user_list.remove(self)
+
+    @classmethod
+    def find_by_user_name(cls,user_name):
+        '''
+        Method that takes in a username and returns a username that matches that username.
+
+        Args:
+            user_name: user_name to search for
+        Returns :
+            Credentials of person that matches the username.
+        '''
+
+        for user in cls.user_list:
+            if user.user_name == user_name:
+                return user
